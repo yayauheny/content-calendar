c8fe3a5ef4316f6db94fd060f52f298dbcc04d86
Evgeny Leshok <work.evles@gmail.com>
2023-08-15 22:21:35 +0300

    Initial commit

---
+++ src/main/java/by/yayauheny/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,13 @@
+package by.yayauheny.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentCalendarApplication.class, args);
+    }
+
+}
